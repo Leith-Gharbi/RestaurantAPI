5e3104dc94ea72e31dd52e25cc816f4a8175ee62
Gharbi <leithgharbi@yahoo.fr>
2021-03-02 06:27:20 +0100

    Ajoutez des fichiers projet.

---
+++ RestaurantAPI/Models/OrderMaster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Models
+{
+    public class OrderMaster
+    {
+        [Key]
+        public long OrderMasterId { get; set; }
+        [Column(TypeName="nvarchar(75)")]
+        public string OrderNumber { get; set; }
+
+        public int CustomerId { get; set; }
+        // We added this line below to set the foreign keys
+        public Customer Customer { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string PMethod { get; set; }
+
+        public decimal GTotal { get; set; }
+
+        public List<OrderDetail> OrderDetail { get; set; }
+
+    }
+}
